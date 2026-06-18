1888d9b8fd1072e9f41c56e73df49b49fea31a98
SeoJin Lee <dltjwls5669@naver.com>
2026-06-18 21:28:02 +0900

    [commit] schema.sql
    데이터베이스 설계

--- 3_Database/스터디과제관리프로그램/schema.sql
+++ 3_Database/스터디과제관리프로그램/schema.sql
@@ -4,7 +4,7 @@ use study_manager;
 
 -- 과목 테이블
 create table subjects(
-	subject_id int not null primary key,
+	subject_id int auto_increment primary key,
     subject_name varchar(50) not null,
     description varchar(200),
     created_at datetime default now()
@@ -12,12 +12,51 @@ create table subjects(
 
 -- 과제 테이블
 create table tasks(
-	task_id int not null primary key, 
+	task_id int auto_increment primary key, 
     subject_id int not null,
     title varchar(100) not null,
     content text not null,
     priority varchar(10),
     status varchar(10),
     due_date date,
-    created_at datetime default now()
+    created_at datetime default now(),
+    
+    foreign key (subject_id) references subjects(subject_id)
 );
+
+-- 스터디 플랜 테이블
+create table study_plans(
+	plan_id int auto_increment primary key,
+    subject_id int not null,
+    plan_title varchar(100) not null,
+    plan_date date,
+    start_time time,
+    end_time time,
+    memo varchar(200),
+    created_at datetime default now(),
+    
+    foreign key (subject_id) references subjects(subject_id)
+);
+
+-- 스터디 로그 테이블
+create table study_logs(
+	log_id int auto_increment primary key,
+    subject_id int not null,
+    study_date date,
+    study_time int,
+    content varchar(300) not null,
+    created_at datetime default now(),
+    
+	foreign key (subject_id) references subjects(subject_id)
+);
+
+-- 과제 메모 테이블
+create table task_memos(
+	memo_id int auto_increment primary key,
+    task_id int not null,
+    memo varchar(300) not null,
+    created_at datetime default now(),
+    
+    foreign key (task_id) references tasks(task_id)
+);
+
